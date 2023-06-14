888393e7da4dd18ca20fd860d0f275188a192784
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-06-14 17:39:53 +0900

    [Silver IV] Title: 개미, Time: 108 ms, Memory: 12880 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
-class Main{
+class Main {
     public static void main(String[] args) {
+        // 첫줄에는 w와 h가 공백을 사이에 두고 주어진다.
+        // 그 다음 줄에는 초기 위치의 좌표값 p와 q가 공백을 사이에 두고 주어진다.
+        // 3번째 줄에는 개미가 움직일 시간 t가 주어진다.
+        //출력은 t 시간 후에 개미의 위치 좌표 (x,y)의 값 x와 y를 공백을 사이에 두고 출력한다.
         Scanner sc = new Scanner(System.in);
         int w = sc.nextInt();
         int h = sc.nextInt();
