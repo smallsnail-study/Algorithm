52608d8a542a8eeb217ca8dbf93df3ddc38f4c71
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-06-15 15:06:37 +0900

    [Silver V] Title: 줄세우기, Time: 396 ms, Memory: 33696 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -2,40 +2,38 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        //첫 줄에 테스트 케이스의 수 P (1 ≤ P ≤ 1000) 가 주어진다.
+        //각 테스트 케이스는 테스트 케이스 번호 T와 20개의 양의 정수가 공백으로 구분되어 주어진다.
+        //20개의 정수는 줄서기를 할 아이들의 키를 줄서기 차례의 순서대로 밀리미터 단위로 나타낸 것이다.
+        //모든 테스트 케이스는 독립적이다.
+        //각각의 테스트 케이스에 대해 테스트 케이스의 번호와 학생들이 뒤로 물러난 걸음 수의 총합을 공백으로 구분하여 출력한다.
         Scanner sc = new Scanner(System.in);
+        int P = sc.nextInt();
+        int[] h = new int[20];
 
-        int P = sc.nextInt();   // 테스트 케이스의 수 P (1 ≤ P ≤ 1000)
-        // O(P)
         while (P-- > 0) {
-            int T = sc.nextInt();   // 테스트 케이스 번호
-            int[] h = new int[20];  // 학생들이 저장되어 있는 배열
-            // O(N)
-            for (int i = 0; i < 20; i++)
-                h[i] = sc.nextInt();
-
-            int cnt = 0;    // 뒤로 물러난 걸음 수 총합
-            int[] sorted = new int[20]; // 학생들이 정렬되어 있는 배열
-            // O(N^)
+            int T = sc.nextInt();
             for (int i = 0; i < 20; i++) {
-                // 1. 줄 서 있는 학생들 중에 자신보다 큰 학생을 찾는다.
+                h[i] = sc.nextInt();
+            }
+
+            int cnt = 0;
+            int[] sorted = new int[20];
+            for (int i = 0; i < 20; i++) {
                 boolean find = false;
-                for (int j = 0; j < i; j++)
+                for (int j = 0; j < i; j++) {
                     if (sorted[j] > h[i]) {
-                        // 2. 찾았다면, 그 학생의 앞에 선다.
-                        // 3. 그 학생과 그 학생 뒤의 학생이 모두 한칸씩 물러난다.
                         find = true;
-                        for (int k = i - 1; k >= j; k--) {// 나보다 큰 학생은 j번째에 위치 / 그 뒤의 학생들은 내가 i번째에 있으므로 i-1번째까지 위치/
-                            // 배열의 특징 때문에 뒤에서부터 계산해줘야 한다.
-                            sorted[k + 1] = sorted[k];
+                        for (int k = i-1; k >= j ; k--) {
+                            sorted[k+1] = sorted[k];
                             cnt++;
                         }
                         sorted[j] = h[i];
                         break;
                     }
-                // 1-1. 찾지 못했다면, 줄의 가장 뒤에 선다.
+                }
                 if (!find) sorted[i] = h[i];
             }
-            // 전체 시간복잡도 : O(P * N^)
             System.out.println(T + " " + cnt);
         }
     }
