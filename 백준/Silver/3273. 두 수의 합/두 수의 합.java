7f30d3d3ca81b8ad1ea7014384600f30f58301c6
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-06-16 16:14:56 +0900

    [Silver III] Title: 두 수의 합, Time: 220 ms, Memory: 25908 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,26 +1,31 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-//        첫째 줄에 수열의 크기 n이 주어진다.
-//        다음 줄에는 수열에 포함되는 수가 주어진다.
-//        셋째 줄에는 x가 주어진다. (1 ≤ n ≤ 100000, 1 ≤ x ≤ 2000000)
-        int N = sc.nextInt();
-        int[] a = new int[N];
-        for(int i = 0; i < N; i++)
-            a[i] = sc.nextInt();
-        int X = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        boolean[] exist = new boolean[1000001];
-        for(int i = 0; i < N; i++)
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int x = Integer.parseInt(br.readLine());
+        boolean[] exist = new boolean[1000000];
+        for (int i = 0; i < n; i++) {
             exist[a[i]] = true;
+        }
 
         int ans = 0;
-        for (int i = 0; i <= (X - 1) / 2; i++) {
-            if (i <= 1000000 && X - i <= 1000000)
-                ans += (exist[i] && exist[X - i]) ? 1 : 0;
+        for (int i = 0; i < n; i++) {
+            int pairValue = x - a[i];
+            if (1<= pairValue && pairValue <= 1000000)
+                ans += exist[pairValue]?1:0;
         }
-        System.out.println(ans);
+        System.out.println(ans / 2);
     }
 }
