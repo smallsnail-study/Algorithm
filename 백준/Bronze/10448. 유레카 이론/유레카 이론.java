e88e739b17239966c4bdf4fe681c379f76ccbfe0
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-06-21 20:33:20 +0900

    [Bronze I] Title: 유레카 이론, Time: 84 ms, Memory: 11840 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -2,29 +2,22 @@ import java.io.*;
 
 class Main {
     static boolean[] isEurekaNumber = new boolean[1001];
-    public static void preprocess() {
 
-        int[] triangleNumbers = new int[50];
-        int triangleNumberCount = 0;
-        for (int i = 1; ; i++) {
+    public static void preprocess() {
+        int[] triangle = new int[50];
+        int triangleCount = 0;
+        for (int i = 1;  ; i++) {
             int triangleNumber = i * (i + 1) / 2;
             if (triangleNumber > 1000) break;
-            triangleNumbers[triangleNumberCount++] = triangleNumber;
+            triangle[triangleCount++] = triangleNumber;
         }
 
-        boolean[] isSumOfTriangleNumber = new boolean[1000];
-        for (int i = 0; i < triangleNumberCount; i++) {
-            for (int j = 0; j < triangleNumberCount; j++) {
-                int sum = triangleNumbers[i] + triangleNumbers[j];
-                if (sum < 1000) isSumOfTriangleNumber[sum] = true;
-            }
-        }
-
-        for (int i = 0; i < 1000; i++) {
-            if (!isSumOfTriangleNumber[i]) continue;
-            for (int j = 0; j < triangleNumberCount; j++) {
-                int sum = i + triangleNumbers[j];
-                if (sum <=1000) isEurekaNumber[sum] = true;
+        for (int i = 0; i < triangleCount; i++) {
+            for (int j = 0; j < triangleCount; j++) {
+                for (int k = 0; k < triangleCount; k++) {
+                    int sum = triangle[i] + triangle[j] + triangle[k];
+                    if (sum <= 1000) isEurekaNumber[sum] = true;
+                }
             }
         }
     }
@@ -35,9 +28,10 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
+
         while (T-- > 0) {
             int K = Integer.parseInt(br.readLine());
-            bw.write(isEurekaNumber[K]?"1":"0");
+            bw.write(isEurekaNumber[K] ? "1" : "0");
             bw.newLine();
         }
         bw.flush();
