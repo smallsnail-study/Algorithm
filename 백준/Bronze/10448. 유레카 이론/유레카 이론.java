f33d4a2b977356f107daebee3ed15d0f71d087d9
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-06-21 20:02:24 +0900

    [Bronze I] Title: 유레카 이론, Time: 88 ms, Memory: 11840 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -1,29 +1,33 @@
 import java.io.*;
 
 class Main {
-    static boolean[] isSumOfTriangleNumber = new boolean[1001];
+    static boolean[] isEurekaNumber = new boolean[1001];
     public static void preprocess() {
+
         int[] triangleNumbers = new int[50];
         int triangleNumberCount = 0;
         for (int i = 1; ; i++) {
-            int triangleNumber = i * (i +1) / 2;
-            if (triangleNumber >= 1000) break;
+            int triangleNumber = i * (i + 1) / 2;
+            if (triangleNumber > 1000) break;
             triangleNumbers[triangleNumberCount++] = triangleNumber;
         }
-//        boolean[] isSumOfTriangleNumber = new boolean[1000];
 
+        boolean[] isSumOfTriangleNumber = new boolean[1000];
         for (int i = 0; i < triangleNumberCount; i++) {
             for (int j = 0; j < triangleNumberCount; j++) {
-                for (int k = 0; k < triangleNumberCount; k++) {
-                    int sum = triangleNumbers[i] + triangleNumbers[j]+triangleNumbers[k];
-                    if (sum <= 1000) isSumOfTriangleNumber[sum] = true;
-                }
+                int sum = triangleNumbers[i] + triangleNumbers[j];
+                if (sum < 1000) isSumOfTriangleNumber[sum] = true;
+            }
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            if (!isSumOfTriangleNumber[i]) continue;
+            for (int j = 0; j < triangleNumberCount; j++) {
+                int sum = i + triangleNumbers[j];
+                if (sum <=1000) isEurekaNumber[sum] = true;
             }
         }
     }
-
-//    static boolean isEurekaNumber = new boolean[1001];
-
     public static void main(String[] args) throws IOException {
         preprocess();
 
@@ -33,7 +37,7 @@ class Main {
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
             int K = Integer.parseInt(br.readLine());
-            bw.write(isSumOfTriangleNumber[K] ? "1" : "0");
+            bw.write(isEurekaNumber[K]?"1":"0");
             bw.newLine();
         }
         bw.flush();
