fe48b9a2a7cce31901d25722fdaa4e789ffa7db9
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-06-27 20:09:28 +0900

    [Silver II] Title: 사탕 게임, Time: 132 ms, Memory: 11692 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+class Main {
+    public static void changeColor(char[][] board, int r1, int c1, int r2, int c2) {
+        char tmp = board[r1][c1];
+        board[r1][c1] = board[r2][c2];
+        board[r2][c2] = tmp;
+    }
+
+    public static int findMaxRow(char[][] board) {
+        int N = board.length;
+        int maxRow = 0;
+        for (int r = 0; r < N; r++) {
+            int len = 1;
+            for (int c = 1; c < N; c++) {
+                if (board[r][c] == board[r][c - 1]) {
+                    len++;
+                } else {
+                    maxRow = Math.max(maxRow, len);
+                    len = 1;
+                }
+            }
+            maxRow = Math.max(maxRow, len);
+        }
+        return maxRow;
+    }
+
+    public static int findMaxColumn(char[][] board) {
+        int N = board.length;
+        int maxColumn = 0;
+        for (int c = 0; c < N; c++) {
+            int len = 1;
+            for (int r = 1; r < N; r++) {
+                if (board[r][c] == board[r - 1][c]) {
+                    len++;
+                } else {
+                    maxColumn = Math.max(maxColumn, len);
+                    len = 1;
+                }
+            }
+            maxColumn = Math.max(maxColumn, len);
+        }
+        return maxColumn;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[][] board = new char[N][N];
+
+        for (int i = 0; i < N; i++) {
+            board[i]= br.readLine().toCharArray();
+        }
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (j + 1 < N && board[i][j] != board[i][j + 1]) {
+                    changeColor(board,i,j,i,j+1);
+                    ans = Math.max(ans, Math.max(findMaxColumn(board), findMaxRow(board)));
+                    changeColor(board,i,j,i,j+1);
+                }
+                if (i + 1 < N && board[i][j] != board[i + 1][j]) {
+                    changeColor(board,i,j,i+1,j);
+                    ans = Math.max(ans, Math.max(findMaxColumn(board),findMaxRow(board)));
+                    changeColor(board,i,j,i+1,j);
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
