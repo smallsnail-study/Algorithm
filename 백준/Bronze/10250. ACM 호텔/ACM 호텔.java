66f4acfa6b139c78ce23bcbf1866f5b92b909cb8
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-07-03 16:45:19 +0900

    [Bronze III] Title: ACM 호텔, Time: 112 ms, Memory: 12292 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,17 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
-            int H = sc.nextInt();   // 층 수
-            int W = sc.nextInt();   // 방 수
-            int N = sc.nextInt();   // 몇번째 손님인지
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken()); // 층 수
+            int W = Integer.parseInt(st.nextToken()); // 방 수
+            int N = Integer.parseInt(st.nextToken()); // 몇 번째 손님인지
+            int YY = (N - 1) % H + 1; // 층수
+            int XX = (N - 1) / H + 1; // 엘리베이터로부터의 거리
 
-            int distance = (N - 1) / H + 1;
-            int floor = (N - 1) % H + 1;
-            System.out.printf("%d%02d\n",floor,distance);
+            System.out.printf("%d%02d\n", YY, XX);
         }
     }
 }
