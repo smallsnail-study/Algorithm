8544086a891ab16b160831742fa4d9429c032065
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-07-03 18:21:48 +0900

    [Silver V] Title: 판화, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/1730. 판화/판화.java
+++ 백준/Silver/1730. 판화/판화.java
@@ -1,41 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        // 입력이 있어야만 받도록 설정(설정하지 않을 경우 RuntimeError발생) -> sc.hasNext()
-        String command = sc.hasNext() ? sc.next() : "";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String command = br.ready() ? br.readLine() : "";
 
         boolean[][] passVertical = new boolean[N][N];
         boolean[][] passHorizontal = new boolean[N][N];
         int curR = 0, curC = 0;
         for (int i = 0; i < command.length(); i++) {
             char cmd = command.charAt(i);
-            if (cmd == 'D') {
-                if (curR == N-1) continue;
-                passVertical[curR][curC] = true;
-                passVertical[curR+1][curC] = true;
-                curR++;
-            }
-            else if (cmd == 'U') {
+            if (cmd == 'U') {
                 if (curR == 0) continue;
                 passVertical[curR][curC] = true;
                 passVertical[curR-1][curC] = true;
                 curR--;
+            } else if (cmd == 'D') {
+                if (curR== N-1) continue;
+                passVertical[curR][curC] = true;
+                passVertical[curR+1][curC] = true;
+                curR++;
             } else if (cmd == 'L') {
                 if (curC == 0) continue;
                 passHorizontal[curR][curC] = true;
                 passHorizontal[curR][curC - 1] = true;
                 curC--;
-            }
-            else {
-               if (curC == N-1) continue;
-               passHorizontal[curR][curC] = true;
-               passHorizontal[curR][curC+1] = true;
-               curC++;
+            } else { // cmd == 'R'
+                if (curC == N-1) continue;
+                passHorizontal[curR][curC] = true;
+                passHorizontal[curR][curC+1] = true;
+                curC++;
             }
         }
+        // 지나지않은 점은 '.' 표기
         for (int i = 0; i < N; i++) {
             String ans = "";
             for (int j = 0; j < N; j++) {
