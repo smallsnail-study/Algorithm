bbd522b5e3afef976214c3af7b230ba91ff18a17
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-07-04 14:39:29 +0900

    [Silver IV] Title: 행운의 바퀴, Time: 76 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.java
+++ 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.java
@@ -8,17 +8,17 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 바퀴 칸 수
-        int K = Integer.parseInt(st.nextToken()); // 바퀴 돌리는 횟수
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
         char[] wheel = new char[N];
-        Arrays.fill(wheel,'?');
+        Arrays.fill(wheel, '?');
 
         int curIndex = 0;
         while (K-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken()); // 회전시켰을 때 화살표가 가리키는 글자가 몇번 바뀌었는지
-            char nextAlphabet = st.nextToken().charAt(0); // 멈췄을때 가리키던 글자
+            int S = Integer.parseInt(st.nextToken());
+            char nextAlphabet = st.nextToken().charAt(0);
 
             int nextIndex = ((curIndex - S) % N + N) % N;
             if (wheel[nextIndex] == '?') wheel[nextIndex] = nextAlphabet;
@@ -29,20 +29,25 @@ class Main {
             curIndex = nextIndex;
         }
 
-        // 모든 회전이 끝난 후 바퀴에 적힌 글자가 모두 다른지 확인하는 배열
         boolean[] used = new boolean[26];
         for (int i = 0; i < N; i++) {
             if (wheel[i] == '?') continue;
-            if (used[wheel[i] - 'A']) {
+            else if (used[wheel[i] - 'A']) {
                 System.out.println("!");
                 return;
             }
             used[wheel[i] - 'A'] = true;
         }
 
-        // 마지막 회전에서 화살표가 가리키는 문자부터 시계방향으로 바퀴에 적어놓은 알파벳을 출력
         for (int i = 0; i < N; i++) {
             System.out.print(wheel[(curIndex + i) % N]);
         }
     }
 }
+
+// 시간복잡도 : O(N + K), O(max(N, K))
+// 아이디어 :
+// Arrays.fill() : 배열 전체를 특정 값으로 채우는 형태 또는 배열의 일부분을 특정 값으로 채우는 형태
+// char 배열 wheel: 바퀴의 각 칸을 저장하는 배열로, 바퀴의 상태를 나타냅니다.
+//boolean 배열 used: 알파벳의 사용 여부를 나타내는 배열로, 중복된 알파벳을 확인하는 데 사용됩니다.
+// 자료구조 : 시뮬레이션
