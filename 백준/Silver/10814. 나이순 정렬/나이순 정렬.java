0cd6170f00d886dd3d634af37655273445393bc2
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-07-06 15:43:56 +0900

    [Silver V] Title: 나이순 정렬, Time: 2184 ms, Memory: 142380 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -5,19 +5,14 @@ class Main {
     static class Member implements Comparable<Member>{
         int age;
         String name;
-        int idx;
-
-        Member(int age, String name, int idx) {
+        Member(int age, String name) {
             this.age = age;
             this.name = name;
-            this.idx = idx;
         }
 
         @Override
         public int compareTo(Member o) {
-            if (age != o.age)
                 return age - o.age;
-            return idx - o.idx;
         }
         // 1. 나이가 적은 회원 먼저
         // 2. 나이가 같으면 먼저 가입한 사람 먼저
@@ -28,7 +23,7 @@ class Main {
         int N = sc.nextInt();
         Member[] members = new Member[N];
         for (int i = 0; i < N; i++) {
-            members[i] = new Member(sc.nextInt(), sc.next(), i);
+            members[i] = new Member(sc.nextInt(), sc.next());
         }
 
         Arrays.sort(members);
