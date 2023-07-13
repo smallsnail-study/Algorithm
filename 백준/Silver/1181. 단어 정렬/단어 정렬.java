9145ca170856be43d0775b35bccaadff8f1e5c3e
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-07-13 12:43:47 +0900

    [Silver V] Title: 단어 정렬, Time: 860 ms, Memory: 23348 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,38 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //첫째 줄에 단어의 개수 N이 주어진다.
+        int N = Integer.parseInt(br.readLine());
+        //둘째 줄부터 N개의 줄에 걸쳐 알파벳 소문자로 이루어진 단어가 한 줄에 하나씩 주어진다.
         String[] words = new String[N];
-        for (int i = 0; i < N; i++) {
-            words[i] = sc.next();
+        for (int i = 0; i < N; i++) {  // O(N)
+            words[i] = br.readLine();
         }
-        // 1. 길이가 짧은 것부터
-        // 2. 길이가 같으면 사전 순으로
-//        Arrays.sort(words, new Comparator<String>() { // 원하는 조건으로 정렬하기 위해 Comparator
-//            @Override
-//            public int compare(String o1, String o2) {
-//                if (o1.length() == o2.length())
-//                    return o1.compareTo(o2); // o1에 대해 o2가 사전순으로 빠른지 뒤인지 확인
-//                return o1.length() - o2.length();
-//            }
-//        });
-        // 위의 자동완성되는 부분을 아래처럼 람다로 간단핟게 할 수 있다.(자동완성기능 제공 안될 시 사용해보기)
-        Arrays.sort(words, (o1, o2) -> {
-            if (o1.length() == o2.length())
-                return o1.compareTo(o2); // o1에 대해 o2가 사전순으로 빠른지 뒤인지 확인
-            return o1.length() - o2.length();
-        });
 
-        // 중복된 단어는 하나만 남기고 제거해야 한다.
+        //조건에 따라 정렬하는 프로그램을 작성하시오.
+        //1. 길이가 짧은 것부터
+        //2. 길이가 같으면 사전 순으로
+        Arrays.sort(words, new Comparator<String>() { // O(N log N)
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length())
+                    return o1.compareTo(o2);
+                return o1.length() - o2.length();
+            }
+        });
+        //단, 중복된 단어는 하나만 남기고 제거해야 한다.
         System.out.println(words[0]);
-        for (int i = 1; i < N; i++) {
-            if (!words[i].equals(words[i - 1]))
+        for (int i = 1; i < N; i++) { // O(N)
+            if (!words[i].equals(words[i-1]))
                 System.out.println(words[i]);
         }
     }
 }
+
+// 시간복잡도 : O(N log N)
+// 아이디어 : Arrays.sort(), Comparator<>(), compare()
+// 자료구조 : 문자열, 정렬
