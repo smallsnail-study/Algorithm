b3bf96a1be00f2660b2509dc69adbb84e78d0f9a
Yunhui Kwak <109578096+smallsnail-study@users.noreply.github.com>
2023-08-04 13:25:02 +0900

    [Silver II] Title: 최대 힙, Time: 1408 ms, Memory: 32832 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,3 +1,9 @@
+// https://www.acmicpc.net/problem/11279
+// 총 N개의 정수 x가 주어질 때, x > 0 이면 배열에 추가, x=0 이면 가장 큰 값 출력(단, 배열이 비어있는 경우 0 출력)
+// 시간복잡도 : O(NlogN)
+// 아이디어 : 우선순위 큐(우선순위가 높은 순)
+// 자료구조 : 우선순위 큐(PriorityQueue)
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,7 +17,7 @@ class Main {
         int n = Integer.parseInt(br.readLine());
 
         PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
-        
+
         while (n-- > 0) {
             int x = Integer.parseInt(br.readLine());
 
