daa4c23b4557070dc317c92120d9e87dfc9e8999
Yunhui Kwak <kwakyh17@naver.com>
2024-06-21 09:40:44 +0900

    [Bronze V] Title: 사칙연산, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,7 +1,17 @@
-a, b = map(int, input().split())
+def sum(a, b):
+    return a+b
+def sub(a, b):
+    return a-b
+def mul(a, b):
+    return a*b
+def div(a, b):
+    return int(a/b)
+def mod(a, b):
+    return a%b
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(int(a/b))
-print(a%b)
+a, b = map(int,input().split())
+print(sum(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
+print(mod(a,b))
