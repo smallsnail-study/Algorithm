2eb180249fbe8b22115c93d2f75be28e297959ba
Yunhui Kwak <kwakyh17@naver.com>
2024-10-07 08:59:08 +0900

    [level 2] Title: 방문 길이, Time: 0.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,11 +1,12 @@
 def solution(dirs):
     answer = set()
-    d = {'U':(0,1), 'D':(0,-1), 'R':(1,0), 'L':(-1,0)}
+    dir = {'U':(0,1), 'D':(0,-1), 'R':(1,0), 'L':(-1,0)}
     x, y = 0, 0
     for i in dirs:
-        nx, ny = x + d[i][0], y + d[i][1]
+        nx, ny = x + dir[i][0], y + dir[i][1]
         if -5 <= nx <= 5 and -5 <= ny <= 5:
             answer.add((x, y, nx, ny))
             answer.add((nx, ny, x, y))
             x, y = nx, ny
+            
     return len(answer)//2
