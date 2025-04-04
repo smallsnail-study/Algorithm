10855b627be3ff97dfd9d2fca3c6d7054a3ce510
Yunhui Kwak <kwakyh17@naver.com>
2025-04-04 14:40:52 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N):
+    result_row = []
+    for j in range(M):
+        result_row.append(A[i][j] + B[i][j])
+    
+    print(' '.join(map(str, result_row)))
