1c49f026b127692662c17c75e7af52ce391564e8
Yunhui Kwak <kwakyh17@naver.com>
2025-04-04 14:07:59 +0900

    [Bronze IV] Title: 모음의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.py
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -1,12 +1,6 @@
 def count_vowels(s):
     vowels = 'aeiou'
-    count = 0
-    
-    for char in s.lower():
-        if char in vowels:
-            count += 1
-    
-    return count
+    return sum(1 for char in s.lower() if char in vowels)
 
 while True:
     str = input()
