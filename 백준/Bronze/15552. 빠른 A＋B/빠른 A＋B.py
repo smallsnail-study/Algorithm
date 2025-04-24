f5e9f29842b601bee77224634b6670bae79a7753
Yunhui Kwak <kwakyh17@naver.com>
2025-04-24 10:46:28 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1180 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -1,12 +1,9 @@
 import sys
-my_input = sys.stdin.readline
-N = int(my_input())
 
-pairs = [ list(map(int, my_input().split())) for _ in range(N) ]
+input = sys.stdin.readline
 
-output = [0] * N
+n = int(input())
 
-for i in range(N):
-    output[i] = sum(pairs[i])
-
-print(*output, sep="\n")
+for _ in range(n):
+    a, b = map(int, input().split())
+    print(a + b)
