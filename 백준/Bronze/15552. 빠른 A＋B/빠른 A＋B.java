df7b51b8d9c9860fd7a3a11fba86a50632f7c25a
Yunhui Kwak <kwakyh17@naver.com>
2025-04-24 11:06:41 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 876 ms, Memory: 289224 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,20 +1,18 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int t = Integer.parseInt(br.readLine());
-
-        for (int i=0;i<t;i++){
-            st = new StringTokenizer(br.readLine()," ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            bw.write(a+b+"\n");
+        
+        int n = Integer.parseInt(br.readLine());
+        while(n-- > 0) {
+            String[] nums = br.readLine().split(" ");
+            int a = Integer.parseInt(nums[0]);
+            int b = Integer.parseInt(nums[1]);
+            bw.write(a + b + "\n");
         }
         bw.flush();
+        bw.close();
     }
 }
