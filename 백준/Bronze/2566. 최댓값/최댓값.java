c9a465bf2ffc6d1720dad165d6b7358e5fc63329
Yunhui Kwak <kwakyh17@naver.com>
2025-04-29 12:10:56 +0900

    [Bronze III] Title: 최댓값, Time: 192 ms, Memory: 18456 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int[][] arr = new int[9][9];
-        int maxNum = -1;
+        int maxValue = -1;
         int maxRow = 0;
         int maxCol = 0;
         
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 arr[i][j] = sc.nextInt();
-                if(arr[i][j] > maxNum) {
-                    maxNum = arr[i][j];
+                if (arr[i][j] > maxValue) {
+                    maxValue = arr[i][j];
                     maxRow = i + 1;
                     maxCol = j + 1;
                 }
             }
         }
-        
-        System.out.println(maxNum);
+        System.out.println(maxValue);
         System.out.println(maxRow + " " + maxCol);
     }
 }
