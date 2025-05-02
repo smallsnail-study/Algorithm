b9615f10f883988bff4e0a6c6376943e699dedb8
Yunhui Kwak <kwakyh17@naver.com>
2025-05-02 09:59:32 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,7 +1,6 @@
-numbers = [int(input()) for _ in range(9)]
-
-max_value = max(numbers)
-max_index = numbers.index(max_value)+1
+nums = [int(input()) for _ in range(9)]
+max_value = max(nums)
+max_index = nums.index(max_value) + 1
 
 print(max_value)
 print(max_index)
