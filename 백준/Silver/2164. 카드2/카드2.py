ab2a11669b27253534cbebcf6853aa3b7a73b433
Yunhui Kwak <kwakyh17@naver.com>
2025-05-28 11:18:11 +0900

    [Silver IV] Title: 카드2, Time: 204 ms, Memory: 51848 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,18 +1,14 @@
 import sys
 from collections import deque
 
-n = int(sys.stdin.readline().rstrip())
+N = int(sys.stdin.readline())
+dq = deque()
 
-queue = deque()
+for i in range(N):
+    dq.append(i+1)
 
-# 큐의 맨 뒤에 1부터 n까지의 숫자를 삽입
-for i in range(n):
-    queue.append(i + 1)
+while len(dq) > 1:
+    dq.popleft()
+    dq.append(dq.popleft())
 
-# 큐의 크기가 1보다 클 때, 큐의 가장 앞을 삭제, 
-# 그리고 다시 가장 앞을 가장 뒤에 삽입 반복
-while len(queue) > 1:
-    queue.popleft()
-    queue.append(queue.popleft())
-
-print(queue.pop())
+print(dq[0])
