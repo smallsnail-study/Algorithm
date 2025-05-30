05938bfb68fffc11c5d67acb4b62c990e7808b38
Yunhui Kwak <kwakyh17@naver.com>
2025-05-30 11:38:41 +0900

    [Silver IV] Title: 수 찾기, Time: 1184 ms, Memory: 58500 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,33 +1,23 @@
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-
+        
+        int n = Integer.parseInt(br.readLine());
+        HashSet<Integer> nums = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            arr.add(Integer.parseInt(st.nextToken()));
+        while(n-- > 0) {
+            nums.add(Integer.parseInt(st.nextToken()));
         }
-
-        Collections.sort(arr);
-        int M = Integer.parseInt(br.readLine());
+        
+        int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < M; i++) {
-            if(Collections.binarySearch(arr, Integer.parseInt(st.nextToken()))>=0) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
+        while(m-- > 0) {
+            int num = Integer.parseInt(st.nextToken());
+            System.out.println(nums.contains(num) ? 1 : 0);
         }
+        br.close();
     }
 }
