878ffd1997114d361cc56b45372367c0e9883271
Yunhui Kwak <kwakyh17@naver.com>
2025-06-05 10:46:29 +0900

    [Silver III] Title: 두 수의 합, Time: 560 ms, Memory: 44968 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -3,20 +3,19 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int target = sc.nextInt();
+        int n = Integer.parseInt(sc.nextLine());
+        int[] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int target = Integer.parseInt(sc.nextLine());
+        
         System.out.print(countPairs(arr, target));
+        sc.close();
     }
     
     private static int countPairs(int[] arr, int target) {
         Set<Integer> seen = new HashSet<>();
         int count = 0;
         
-        for(int num : arr) {
+        for (int num : arr) {
             int complement = target - num;
             if (seen.contains(complement)) {
                 count++;
