6bec2c88af4b0cc3167a611898877544ffd7a9d3
Yunhui Kwak <kwakyh17@naver.com>
2025-06-11 09:09:24 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,13 +1,9 @@
 n = int(input())
-dp = {}
-def fibo(n):
-    if n in dp:
-        return dp[n]
-    
-    if n <= 1:
-        return n
-    
-    cur = fibo(n-1) + fibo(n-2)
-    dp[n] = cur
-    return dp[n]
-print(fibo(n))
+a, b = 0, 1
+
+if n == 0:
+    print(a)
+else:
+    for _ in range(2, n+1):
+        a, b = b, a+b
+    print(b)
